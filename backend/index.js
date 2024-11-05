1ddc32ad8288200b172104e1e5197166aef8d0dc
Shikha Gupta <shikha912513@gmail.com>
2024-11-06 01:28:15 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -1,88 +1,4 @@
 
-// import express from "express";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import connectDB from "./utils/db.js";
-// import userRoute from "./routes/user.route.js";
-// import companyRoute from "./routes/company.route.js";
-// import jobRoute from "./routes/job.route.js";
-// import applicationRoute from "./routes/application.route.js";
-
-// dotenv.config({});
-
-// const app = express();
-
-// // Middleware
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
-
-// // CORS Configuration
-// const corsOptions = {
-//   origin: [
-//     'http://localhost:5173',  // For local development
-//     'https://career-bloom-hub.vercel.app'  // Your deployed frontend URL
-//   ],
-//   credentials: true,  // To allow cookies
-// };
-// app.use(cors(corsOptions));
-// // API routes
-// app.use("/api/v1/user", userRoute);
-// app.use("/api/v1/company", companyRoute);
-// app.use("/api/v1/job", jobRoute);
-// app.use("/api/v1/application", applicationRoute);
-
-// // Start the server with Socket.io
-// const PORT = process.env.PORT || 3000;
-// server.listen(PORT, () => {
-//   connectDB();
-//   console.log(`Server running at port ${PORT}`);
-// });
-
-
-// import express from "express";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import connectDB from "./utils/db.js";
-// import userRoute from "./routes/user.route.js";
-// import companyRoute from "./routes/company.route.js";
-// import jobRoute from "./routes/job.route.js";
-// import applicationRoute from "./routes/application.route.js";
-
-// dotenv.config();
-
-// const app = express();
-
-// // Middleware
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
-
-// const corsOptions = {
-//     origin: [
-//         'http://localhost:5173',  // For local development
-//         'https://career-bloom-hub.vercel.app'  // Your deployed frontend URL
-//     ],
-//     credentials: true  // To allow cookies
-// };
-
-// app.use(cors(corsOptions));
-
-// const PORT = process.env.PORT || 3000;
-
-// // APIs
-// app.use("/api/v1/user", userRoute);
-// app.use("/api/v1/company", companyRoute);
-// app.use("/api/v1/job", jobRoute);
-// app.use("/api/v1/application", applicationRoute);
-
-// app.listen(PORT, () => {
-//     connectDB();
-//     console.log(`Server running at port ${PORT}`);
-// });
-
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -92,11 +8,12 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config({});
 
 const app = express();
-
+const _dirname = path.resolve();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -105,6 +22,7 @@ const corsOptions = {
     origin:'http://localhost:5173',
     credentials:true
 }
+
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
@@ -117,7 +35,10 @@ app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
 
-
+app.use(express.static(path.join(_dirname,"/frontend/dist")));
+app.get('*',(_,res)=>{
+  res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+})
 app.listen(PORT,()=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);
